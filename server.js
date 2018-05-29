100162fd6496e85919e0a101f958a3802b603761
regentsdarn <39726710+regentsdarn@users.noreply.github.com>
2018-05-29 15:47:40 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,289 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+// 'client.on('message')' commands are triggered when the
+// specified message is read in a text channel that the bot is in.
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('Pong');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'sepi') {
+    message.reply('Ramein yuk! Bantu promosi server Old Player dengan cara membagikan link http://discord.gg/echG7wd');    
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Sepi') {
+    message.reply('Ramein yuk! Bantu promosi server Old Player dengan cara membagikan link http://discord.gg/echG7wd');    
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Ping') {
+    message.reply('Pong');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'halo') {
+    message.reply('Halo');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Halo') {
+    message.reply('Halo');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'woy') {
+    message.reply('Woy');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Woy') {
+    message.reply('Woy');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'hey') {
+    message.reply('Hey');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Hey') {
+    message.reply('Hey');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'p') {
+    message.reply('P');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'P') {
+    message.reply('P');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'fak') {
+    message.reply('Gak boleh ngomong kasar!');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'op How are you?') {
+    message.reply('I am fine. How about you?');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'op Are you there?') {
+    message.reply('Yes, I am right here');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Mabar') {
+    message.reply('Kuy lah, carry saya ya');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'op Who is your creator?') {
+    message.reply('Regents Darn');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'op Siapa pencipta lu?') {
+    message.reply('Regents Darn');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'op Masih hidup?') {
+    message.reply('Apa maksudmu? Tentu saja aku masih hidup');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'op Siapa yang kamu suka?') {
+    message.reply('Satu satunya member cewek di sini cuma Putriayu, jadi saya suka dia');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'op Siapa yg kamu suka?') {
+    message.reply('Satu satunya member cewek di sini cuma Putriayu, jadi saya suka dia');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'op Siapa yg lu suka?') {
+    message.reply('Satu satunya member cewek di sini cuma Putriayu, jadi saya suka dia');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Mabar kuy') {
+    message.reply('Kuy');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Kapan buka?') {
+    message.reply('Ntar lagi kok');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Cih') {
+    message.reply('Jangan meludah sembarangan!');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Cuih') {
+    message.reply('Jangan meludah sembarangan!');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Anjing') {
+    message.reply('Jangan ngomong kasar!');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'op Are you gay?') {
+    message.reply('No');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Miskin') {
+    message.reply('Gak boleh menghina orang!');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Tarawih woy') {
+    message.reply('Siap kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Teraweh woy') {
+    message.reply('Siap kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Teraweh woy!') {
+    message.reply('Siap kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Selamat pagi') {
+    message.reply('Pagi kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Selamat pagi!') {
+    message.reply('Pagi kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Selamat siang') {
+    message.reply('Siang kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Selamat siang!') {
+    message.reply('Siang kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Selamat sore') {
+    message.reply('Sore kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Selamat sore!') {
+    message.reply('Sore kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Selamat malam') {
+    message.reply('Malam kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Selamat malam!') {
+    message.reply('Malam kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Akhirnya gw chicken pak') {
+    message.reply('Selamat kakak');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Lagunya bikin ngantuk') {
+    message.reply('Mending dengerin ceramah agama aja');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'tes') {
+    message.reply('Tos');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Tes') {
+    message.reply('Tos');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'you') {
+    message.reply('What?');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'You') {
+    message.reply('What?');
+  }
+});
+
+client.on('message', message => {
+  if (message.content === 'Bukber kuy') {
+    message.reply('Ayo kak');
+  }
+});
+
+client.login("NDUwOTA4NjQwODY2NDY3ODQ5.De6HSQ.FvsUHqmsTzTE-7StxVyClSqiM4E");
